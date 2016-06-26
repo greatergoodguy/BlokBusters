26bcd2aa276e7d0ea6a2d4b173818c1aeaedbbd3
greatergoodguy <xuetomyu@gmail.com>
2016-06-26 00:12:41 -0500

    Finished setting up multiplayer project from scratch

---
+++ Assets/Blok Busters/Experiments/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		var hit = collision.gameObject;
+		var hitCombat = hit.GetComponent<Combat>();
+		if (hitCombat != null)
+		{
+			hitCombat.TakeDamage(10);
+			Destroy(gameObject);
+		}
+	}
+}
