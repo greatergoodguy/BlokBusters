fb7bad44d2c01f48b825b573f4c1ce097f56cfa4
greatergoodguy <xuetomyu@gmail.com>
2016-06-25 22:34:21 -0500

    Added bullets to the network experiment

--- Assets/Blok Busters/Experiments/PlayerMove.cs
+++ Assets/Blok Busters/Experiments/PlayerMove.cs
@@ -3,6 +3,13 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour {
 
+	public GameObject bulletPrefab;
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
 	void Update() {
 
 		if (!isLocalPlayer) {
@@ -13,5 +20,25 @@ public class PlayerMove : NetworkBehaviour {
 		var z = Input.GetAxis("Vertical")*0.1f;
 
 		transform.Translate(x, 0, z);
+
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			CmdFire();
+		}
+	}
+
+	[Command]
+	void CmdFire() {
+		// create the bullet object from the bullet prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			transform.position - transform.forward,
+			Quaternion.identity);
+
+		// make the bullet move away in front of the player
+		bullet.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+
+		// make bullet disappear after 2 seconds
+		Destroy(bullet, 2.0f);        
 	}
 }
