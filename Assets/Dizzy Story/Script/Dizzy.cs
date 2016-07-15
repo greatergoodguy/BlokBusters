cbf71c56b710ea783e21826fd66f3fc72577d4df
greatergoodguy <xuetomyu@gmail.com>
2016-07-14 21:08:10 -0500

    Added winner screen after hitting velocity of 25

--- Assets/Dizzy Story/Script/Dizzy.cs
+++ Assets/Dizzy Story/Script/Dizzy.cs
@@ -7,11 +7,12 @@ public class Dizzy : MonoBehaviour {
 
 	Rigidbody2D rigidBody;
 	bool isFacingRight = true;
-	float maxSpeed = 10f;
+	float maxSpeedX = 14f;
 	float additiveMovementForce = 60f;
 	float jumpForce = 1000f;
 
 	bool isGrounded = false;
+	float velocityPreviousFrame;
 
 	void Awake() {
 		rigidBody = GetComponent<Rigidbody2D>();
@@ -19,18 +20,17 @@ public class Dizzy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		float axisHorizontal 		= Input.GetAxis("Horizontal");
 		float velXCurrent 			= rigidBody.velocity.x;
 		float resistanceMultiplier 	= 3;
 
-		if (velXCurrent <= maxSpeed && velXCurrent >= -maxSpeed) {
+		if (velXCurrent <= maxSpeedX && velXCurrent >= -maxSpeedX) {
 			rigidBody.AddForce(new Vector2(axisHorizontal * additiveMovementForce, 0));
 		}
-		else if (velXCurrent <= -maxSpeed && axisHorizontal > 0) {
+		else if (velXCurrent <= -maxSpeedX && axisHorizontal > 0) {
 			rigidBody.AddForce(new Vector2(axisHorizontal * additiveMovementForce, 0));
 		}
-		else if (velXCurrent >= maxSpeed && axisHorizontal < 0) {
+		else if (velXCurrent >= maxSpeedX && axisHorizontal < 0) {
 			rigidBody.AddForce(new Vector2(axisHorizontal * additiveMovementForce, 0));
 		}
 
@@ -41,9 +41,15 @@ public class Dizzy : MonoBehaviour {
 			Flip();
 		}
 
-		if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+//		if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+//			rigidBody.AddForce(new Vector2(0f, jumpForce));
+//		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
 			rigidBody.AddForce(new Vector2(0f, jumpForce));
 		}
+			
+		velocityPreviousFrame = rigidBody.velocity.magnitude;
 	}
 
 	private void Flip() {
@@ -55,10 +61,25 @@ public class Dizzy : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
+		Toolbox.Log("velocityPreviousFrame: " + velocityPreviousFrame);
 		isGrounded = true;
+
+		if (velocityPreviousFrame > 25.0f) {
+			StartCoroutine(ShowWinnerScreenAndStartNextLevel());
+		}
 	}
 
 	void OnCollisionExit2D(Collision2D collision) {
 		isGrounded = false;
 	}
+
+	IEnumerator ShowWinnerScreenAndStartNextLevel() {
+		GameObject goUIWinner = GameObject.Find("UI Winner");
+		goUIWinner.transform.Find("Panel").gameObject.SetActive(true);
+
+		yield return new WaitForSeconds(2);
+
+		Application.LoadLevel("Level 1");
+	}
 }
+
