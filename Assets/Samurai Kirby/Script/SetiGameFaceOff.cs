c84e47800dea5f1cf23b7d61123814c219934876
greatergoodguy <xuetomyu@gmail.com>
2016-08-21 15:57:21 -0500

    Added better suspense music

--- Assets/Samurai Kirby/Script/SetiGameFaceOff.cs
+++ Assets/Samurai Kirby/Script/SetiGameFaceOff.cs
@@ -42,10 +42,10 @@ public class SetiGameFaceOff : SeTi_Base {
 
 		ActorSFX.Instance.Play(5);
 		uiFaceoff.Engage(1.5f);
-		yield return new WaitForSeconds(2.0f);
+		yield return new WaitForSeconds(3.0f);
 
-		uiFaceoffRemove.Engage(1.5f);
-		yield return new WaitForSeconds(1.5f);
+		uiFaceoffRemove.Engage(2.0f);
+		yield return new WaitForSeconds(2.0f);
 
 		uiFader.UnFade(1.5f);
 		yield return new WaitForSeconds(1.5f);
